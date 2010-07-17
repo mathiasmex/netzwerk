1f79bb5c9d77d8d0f0ca7007782f1350161e1ead
Mathias <mathiasmex@gmail.com>
2010-07-17 15:57:02 +0200

    included groups, events, i18n, geolocate

--- db/migrate/20080911223251_add_whitelist_bool_preferences.rb
+++
@@ -1,9 +0,0 @@
-class AddWhitelistBoolPreferences < ActiveRecord::Migration
-  def self.up
-    add_column :preferences, :whitelist, :boolean, :default => false
-  end
-
-  def self.down
-    remove_column :preferences, :whitelist
-  end
-end
--- db/migrate/20090211164230_add_address_to_events.rb
+++
@@ -1,9 +0,0 @@
-class AddAddressToEvents < ActiveRecord::Migration
-  def self.up
-    add_column :events, :full_address, :string
-  end
-
-  def self.down
-    remove_column :events, :full_address
-  end
-end
